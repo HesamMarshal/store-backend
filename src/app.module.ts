d051d1479b3407d4e702a8b2175c91e111b94c7e
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-09 13:03:26 +0330

    add user for database

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { TypeOrmModule } from "@nestjs/typeorm";
       type: "mysql",
       host: "localhost",
       port: 3306,
+      username: "root",
       password: "",
       database: "onlinestore",
       autoLoadEntities: false,
