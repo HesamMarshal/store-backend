485c737505928c19d6812e63930c6291d5cc40c2
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-09 12:54:51 +0330

    add typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,21 @@ import { AppService } from "./app.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localhost",
+      port: 3306,
+      password: "",
+      database: "onlinestore",
+      autoLoadEntities: false,
+      synchronize: true,
+      entities: [
+        "dist/**/**/**/*.entity{.ts,.js}",
+        "dist/**/**/*.entity{.ts,.js}",
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
