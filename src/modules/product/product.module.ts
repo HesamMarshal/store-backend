56bb3b7abd84bd9d5ebaf07a5b4fa02a1f61f93e
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-10 11:44:18 +0330

    create product module

---
+++ src/modules/product/product.module.ts
@@ -0,0 +1,38 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Product } from "./entites/product.entity";
+import { ProductColor } from "./entites/product-color.entity";
+import { ProductSize } from "./entites/product-size.entity";
+import { ProductDetail } from "./entites/product-detail.entity";
+import { ProductController } from "./controller/product.controller";
+import { ProductSizeController } from "./controller/product-size.controller";
+import { ProductColorController } from "./controller/product-color.controller";
+import { ProductDetailController } from "./controller/product-detail.controller";
+import { ProductService } from "./service/product.service";
+import { ProductColorService } from "./service/product-color.service";
+import { ProductDetailService } from "./service/product-detail.service";
+import { ProductSizeService } from "./service/product-size.service";
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      Product,
+      ProductColor,
+      ProductSize,
+      ProductDetail,
+    ]),
+  ],
+  controllers: [
+    ProductController,
+    ProductSizeController,
+    ProductColorController,
+    ProductDetailController,
+  ],
+  providers: [
+    ProductService,
+    ProductDetailService,
+    ProductSizeService,
+    ProductColorService,
+  ],
+})
+export class ProductModule {}
