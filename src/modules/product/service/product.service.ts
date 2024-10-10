f18ee000bb4cedcf061d80a58969b8d3f8f0ac25
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-10 12:33:09 +0330

    implement create method

--- src/modules/product/service/product.service.ts
+++ src/modules/product/service/product.service.ts
@@ -1,10 +1,47 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { CreateProductDto, UpdateProductDto } from "../dto/product.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Product } from "../entites/product.entity";
+import { DeepPartial, Repository } from "typeorm";
+import { ProductType } from "../enum/type.enum";
+import { toBoolean } from "src/utils/functions";
 
 @Injectable()
 export class ProductService {
-  create(productDto: CreateProductDto) {
-    throw new Error("Method not implemented.");
+  constructor(
+    @InjectRepository(Product) private productRepository: Repository<Product>
+  ) {}
+  async create(productDto: CreateProductDto) {
+    const {
+      title,
+      slug,
+      active_discount,
+      code,
+      content,
+      count,
+      discount,
+      price,
+      type,
+    } = productDto;
+    const productObject: DeepPartial<Product> = {
+      title,
+      content,
+      slug,
+      code,
+      discount,
+      active_discount: toBoolean(active_discount),
+    };
+
+    if (type === ProductType.Single) {
+      Object.assign(productObject, { price, count, type });
+    } else if ([ProductType.Coloring, ProductType.Sizing].includes(type as any))
+      productObject["type"] = type;
+    else throw new BadRequestException("product type is invalid");
+
+    await this.productRepository.save(productObject);
+    return {
+      message: "created product successful",
+    };
   }
 
   find() {
