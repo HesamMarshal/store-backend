a2da64d8435a008f563d0ac60394f7fe73d9c40d
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-11 12:57:31 +0330

    implement service

--- src/modules/product/service/product-color.service.ts
+++ src/modules/product/service/product-color.service.ts
@@ -1,18 +1,150 @@
-import { Injectable } from "@nestjs/common";
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from "@nestjs/common";
 import { AddColorDto, UpdateColorDto } from "../dto/color.dto";
+import { ProductService } from "./product.service";
+import { ProductColor } from "../entites/product-color.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Product } from "../entites/product.entity";
+import { DataSource, Repository } from "typeorm";
+import { toBoolean } from "src/utils/functions";
+import { ProductType } from "../enum/type.enum";
 
 @Injectable()
 export class ProductColorService {
-  create(colorDto: AddColorDto) {
-    throw new Error("Method not implemented.");
+  constructor(
+    private productService: ProductService,
+    @InjectRepository(ProductColor)
+    private productColorRepository: Repository<ProductColor>,
+    @InjectRepository(Product)
+    private productRepository: Repository<Product>,
+    private dataSource: DataSource
+  ) {}
+
+  async create(colorDto: AddColorDto) {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    try {
+      await queryRunner.startTransaction();
+      const {
+        active_discount,
+        count,
+        discount,
+        price,
+        productId,
+        color_code,
+        color_name,
+      } = colorDto;
+      let product = await queryRunner.manager.findOneBy(Product, {
+        id: productId,
+      });
+      if (product.type !== ProductType.Coloring)
+        throw new BadRequestException("product type isn't coloring");
+      if (!product) throw new NotFoundException("not found product");
+      await queryRunner.manager.insert(ProductColor, {
+        count,
+        discount,
+        price,
+        color_code,
+        color_name,
+        active_discount: toBoolean(active_discount),
+        productId,
+      });
+      if (!isNaN(parseInt(count.toString())) && +count > 0) {
+        product.count =
+          parseInt(product.count.toString()) + parseInt(count.toString());
+        await queryRunner.manager.save(Product, product);
+      }
+      await queryRunner.commitTransaction();
+      await queryRunner.release();
+      return {
+        message: "created color of product successful",
+      };
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      await queryRunner.release();
+      throw error;
+    }
   }
-  delete(id: number) {
-    throw new Error("Method not implemented.");
+
+  async find(productId: number) {
+    return this.productColorRepository.find({
+      where: { productId },
+    });
   }
-  update(id: number, colorDto: UpdateColorDto) {
-    throw new Error("Method not implemented.");
+  async findOne(id: number) {
+    const color = await this.productColorRepository.findOne({
+      where: { id },
+    });
+    if (!color) throw new NotFoundException();
+    return color;
   }
-  find(productId: number) {
-    throw new Error("Method not implemented.");
+
+  async update(id: number, colorDto: UpdateColorDto) {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    try {
+      await queryRunner.startTransaction();
+      const {
+        active_discount,
+        count,
+        discount,
+        price,
+        productId,
+        color_code,
+        color_name,
+      } = colorDto;
+      let product = await queryRunner.manager.findOneBy(Product, {
+        id: productId,
+      });
+      if (!product) throw new NotFoundException("not found product");
+      let color = await queryRunner.manager.findOneBy(ProductColor, {
+        id,
+      });
+      if (!color) throw new NotFoundException("not found product");
+      if (color_name) color.color_name = color_name;
+      if (color_code) color.color_code = color_code;
+      if (active_discount) color.active_discount = toBoolean(active_discount);
+      if (discount) color.discount = discount;
+      if (price) color.price = price;
+      let previousCount = color.count;
+      if (count && !isNaN(parseInt(count.toString())) && +count > 0) {
+        product.count =
+          parseInt(product.count.toString()) -
+          parseInt(previousCount.toString());
+        product.count =
+          parseInt(product.count.toString()) + parseInt(count.toString());
+        color.count = count;
+        await queryRunner.manager.save(Product, product);
+      }
+      await queryRunner.manager.save(ProductColor, color);
+      await queryRunner.commitTransaction();
+      await queryRunner.release();
+      return {
+        message: "created color of product successful",
+      };
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      await queryRunner.release();
+      throw error;
+    }
+  }
+
+  async delete(id: number) {
+    const color = await this.findOne(id);
+    if (color.count && color.count > 0) {
+      const product = await this.productRepository.findOneBy({
+        id: color.productId,
+      });
+      product.count =
+        parseInt(product.count.toString()) - parseInt(color.count.toString());
+      await this.productRepository.save(product);
+    }
+    await this.productColorRepository.delete({ id });
+    return {
+      message: "Deleted color of product successful.",
+    };
   }
 }
