69b5bd0b1707e4fde31e29d38dc1b1f3abdff066
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-11 12:57:58 +0330

    bugfix

--- src/modules/product/entites/product.entity.ts
+++ src/modules/product/entites/product.entity.ts
@@ -9,29 +9,41 @@ import {
 import { ProductDetail } from "./product-detail.entity";
 import { ProductSize } from "./product-size.entity";
 import { ProductColor } from "./product-color.entity";
+import { ProductType } from "../enum/type.enum";
 
 @Entity()
 export class Product {
   @PrimaryGeneratedColumn("increment")
   id: number;
+
   @Column()
   title: string;
+
   @Column()
   content: string;
+
   @Column()
   slug: string;
+
   @Column()
   code: string;
-  // @Column({enum: ProductType})
-  // type: string;
+
+  // @Column({ enum: ProductType })
+  @Column()
+  type: string;
+
   @Column({ default: 0 })
   count: number;
+
   @Column({ type: "decimal", nullable: true })
   price: number;
+
   @Column({ type: "decimal", nullable: true, default: 0 })
   discount: number;
+
   @Column({ nullable: true, default: false })
   active_discount: boolean;
+
   @CreateDateColumn()
   created_at: Date;
 
